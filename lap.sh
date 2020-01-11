633cb85cfb2d67b246063438226e82379e8b7f0b
chrodriguezs <cdrs1991@gmail.com>
2020-01-10 21:10:09 -0500

    Add files via upload

--- lap.sh
+++ lap.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
- 
-#######################################
-# Bash script to install an AMP stack and PHPMyAdmin plus tweaks. For Debian based systems.
-# Written by @AamnahAkram from http://aamnah.com
-# In case of any errors (e.g. MySQL) just re-run the script. Nothing will be re-installed except for the packages with errors.
-#######################################
- 
 # Update packages
 echo -e "\n Updating System.."
 sudo apt-get update
@@ -13,9 +6,9 @@ sudo apt-get update
 echo -e "\n Installing Apache2"
 sudo apt-get install -y apache2
 echo -e "\n Installing PHP & Requirements"
-sudo apt-get install -y libapache2-mod-php7.0 php7.0 php7.0-common php7.0-curl php7.0-dev php-gd php7.0-intl  php-pear php-mcrypt php7.0-mysql php7.0-ps php7.0-pspell php7.0-recode
+sudo apt-get install -y libapache2-mod-php7.0 php7.0 php7.0-common php7.0-curl php7.0-dev php7.0-intl  php-pear php-mcrypt php7.0-mysql php7.0-ps php7.0-pspell
 echo -e "\n Verifying installs$Color_Off"
-sudo apt-get install -y apache2 libapache2-mod-php7.0 php7.0 mysql-server php-pear php7.0-mysql mysql-client mysql-server php7.0-mysql php-gd
+sudo apt-get install -y apache2 libapache2-mod-php7.0 php7.0 mysql-server php-pear php7.0-mysql mysql-client mysql-server php7.0-mysql
 ## TWEAKS and Settings
 # Permissions
 echo -e "\n Permissions for /var/www"
@@ -24,7 +17,6 @@ echo -e "$Green \n Permissions have been set"
 # Enabling Mod Rewrite, required for WordPress permalinks and .htaccess files
 echo -e "\n Enabling Modules"
 sudo a2enmod rewrite
-sudo phpenmod mcrypt
 # Restart Apache
 echo -e "\n Restarting Apache"
 sudo service apache2 restart
