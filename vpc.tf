46b7c6f876074872c5684e4afec35144624bf202
chrodriguezs <cdrs1991@gmail.com>
2020-01-11 03:32:05 -0500

    Add files via upload

--- vpc.tf
+++ vpc.tf
@@ -1,19 +1,6 @@
-# Internet VPC
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  instance_tenancy     = "default"
-  enable_dns_support   = "true"
-  enable_dns_hostnames = "true"
-  enable_classiclink   = "false"
-  tags = {
-    Name = "main",
-    Project = "DEMO_Ene20"
-  }
-}
-
 # Subnets
 resource "aws_subnet" "main-public-1" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = "vpc-043574869874c08de"
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "eu-west-1a"
@@ -25,7 +12,7 @@ resource "aws_subnet" "main-public-1" {
 }
 
 resource "aws_subnet" "main-public-2" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = "vpc-043574869874c08de"
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "eu-west-1b"
@@ -37,7 +24,7 @@ resource "aws_subnet" "main-public-2" {
 }
 
 resource "aws_subnet" "main-public-3" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = "vpc-043574869874c08de"
   cidr_block              = "10.0.3.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "eu-west-1c"
@@ -49,7 +36,7 @@ resource "aws_subnet" "main-public-3" {
 }
 
 resource "aws_subnet" "main-private-1" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = "vpc-043574869874c08de"
   cidr_block              = "10.0.4.0/24"
   map_public_ip_on_launch = "false"
   availability_zone       = "eu-west-1a"
@@ -61,7 +48,7 @@ resource "aws_subnet" "main-private-1" {
 }
 
 resource "aws_subnet" "main-private-2" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = "vpc-043574869874c08de"
   cidr_block              = "10.0.5.0/24"
   map_public_ip_on_launch = "false"
   availability_zone       = "eu-west-1b"
@@ -73,7 +60,7 @@ resource "aws_subnet" "main-private-2" {
 }
 
 resource "aws_subnet" "main-private-3" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = "vpc-043574869874c08de"
   cidr_block              = "10.0.6.0/24"
   map_public_ip_on_launch = "false"
   availability_zone       = "eu-west-1c"
@@ -86,7 +73,7 @@ resource "aws_subnet" "main-private-3" {
 
 # Internet GW
 resource "aws_internet_gateway" "main-gw" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = "vpc-043574869874c08de"
 
   tags = {
     Name = "main",
@@ -96,7 +83,7 @@ resource "aws_internet_gateway" "main-gw" {
 
 # route tables
 resource "aws_route_table" "main-public" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = "vpc-043574869874c08de"
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main-gw.id
