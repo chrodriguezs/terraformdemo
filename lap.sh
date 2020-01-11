4620cf83b8059c0d65d177d3d3b09e8c82de0c95
chrodriguezs <cdrs1991@gmail.com>
2020-01-10 20:54:09 -0500

    Add files via upload

--- lap.sh
+++ lap.sh
@@ -1,20 +1,52 @@
 #!/bin/sh
 
-sudo apt-get update
+#######################################
+# Bash script to install an AMP stack and PHPMyAdmin plus tweaks. For Debian based systems.
+# Written by @AamnahAkram from http://aamnah.com
 
-## Install AP
+# In case of any errors (e.g. MySQL) just re-run the script. Nothing will be re-installed except for the packages with errors.
+#######################################
+
+#COLORS
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+
+# Update packages
+echo -e "$Cyan \n Updating System.. $Color_Off"
+sudo apt-get update -y
+
+## Install AMP
+echo -e "$Cyan \n Installing Apache2 $Color_Off"
 sudo apt-get install apache2 apache2-doc apache2-utils -y
+
+echo -e "$Cyan \n Installing PHP & Requirements $Color_Off"
 sudo apt-get install libapache2-mod-php7.0 php7.0 php7.0-common php7.0-curl php7.0-dev php7.0-gd php7.0-intl php7.0-openssl php7.0-idn php-pear php7.0-imagick php7.0-mcrypt php7.0-mysql php7.0-ps php7.0-pspell php7.0-recode php7.0-xsl -y
 
+echo -e "$Cyan \n Verifying installs$Color_Off"
+sudo apt-get install apache2 libapache2-mod-php7.0 php7.0 mysql-server php-pear php7.0-mysql mysql-client mysql-server php7.0-mysql php7.0-gd -y
 
+## TWEAKS and Settings
 # Permissions
+echo -e "$Cyan \n Permissions for /var/www $Color_Off"
 sudo chown -R www-data:www-data /var/www
+echo -e "$Green \n Permissions have been set $Color_Off"
 
 # Enabling Mod Rewrite, required for WordPress permalinks and .htaccess files
+echo -e "$Cyan \n Enabling Modules $Color_Off"
 sudo a2enmod rewrite
 sudo php7.0enmod mcrypt
 
+# Restart Apache
+echo -e "$Cyan \n Restarting Apache $Color_Off"
 sudo service apache2 restart
 
 # Enabling Apache
+echo -e "$Cyan \n Enabling Apache $Color_Off"
 sudo systemctl enable apache2.service
