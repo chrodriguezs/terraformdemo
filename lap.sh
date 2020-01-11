3af287c75005a3eb280a368f1eece6c87ec52c80
chrodriguezs <cdrs1991@gmail.com>
2020-01-11 00:13:37 -0500

    Add files via upload

--- lap.sh
+++ lap.sh
@@ -1,25 +1,11 @@
 #!/bin/sh
-# Update packages
-echo -e "\n Updating System.."
-echo $(sudo apt-get update)
-## Install AMP
-echo -e "\n Installing Apache2"
-sudo apt-get install -y apache2
-echo -e "\n Installing PHP & Requirements"
-sudo apt-get install -y php php-gd php-bcmath php-intl php-openssl php-mbstring php-curl php-mysql
-echo -e "\n Verifying installs$Color_Off"
-sudo apt-get install -y apache2 php php-gd php-bcmath php-intl php-openssl php-mbstring php-curl php-mysql
-## TWEAKS and Settings
-# Permissions
-echo -e "\n Permissions for /var/www"
-sudo chown -R www-data:www-data "/var/www"
-echo -e "$Green \n Permissions have been set"
-# Enabling Mod Rewrite, required for WordPress permalinks and .htaccess files
-echo -e "\n Enabling Modules"
+echo "Updating Libraries"
+sudo apt-get update
+echo "Installing Apache"
+sudo apt-get install -y apache2 apache2-bin apache2-utils
+echo "Installing PHP & Complements"
+sudo apt-get install -y php7.0 php7.0-gd php7.0-bcmath php7.0-intl php7.0-mbstring php7.0-curl
+echo "Enable rewrite & Restart and Enable Apache Service"
 sudo a2enmod rewrite
-# Restart Apache
-echo -e "\n Restarting Apache"
-sudo service apache2 restart
-# Enabling Apache
-echo -e "\n Enabling Apache"
-sudo systemctl enable apache2.service
+sudo Service apache2 restart
+sudo systemclt enable apache2.service
