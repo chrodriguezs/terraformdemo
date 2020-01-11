71301928a54af2021504b3504b1c3c79edcff954
chrodriguezs <cdrs1991@gmail.com>
2020-01-10 21:16:07 -0500

    Add files via upload

--- lap.sh
+++ lap.sh
@@ -6,9 +6,9 @@ sudo apt-get update
 echo -e "\n Installing Apache2"
 sudo apt-get install -y apache2
 echo -e "\n Installing PHP & Requirements"
-sudo apt-get install -y libapache2-mod-php7.0 php7.0 php7.0-common php7.0-curl php7.0-dev php7.0-intl  php-pear php-mcrypt php7.0-mysql php7.0-ps php7.0-pspell
+sudo apt-get install -y php php-gd php-bcmath php-intl php-openssl php-mbstring php-curl php-mysql
 echo -e "\n Verifying installs$Color_Off"
-sudo apt-get install -y apache2 libapache2-mod-php7.0 php7.0 mysql-server php-pear php7.0-mysql mysql-client mysql-server php7.0-mysql
+sudo apt-get install -y apache2 php php-gd php-bcmath php-intl php-openssl php-mbstring php-curl php-mysql
 ## TWEAKS and Settings
 # Permissions
 echo -e "\n Permissions for /var/www"
