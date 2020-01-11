8ede2daa5bda460e396276a90917ae1d4f9be0a1
chrodriguezs <cdrs1991@gmail.com>
2020-01-11 02:50:35 -0500

    Add files via upload

--- rds.tf
+++ rds.tf
@@ -27,6 +27,7 @@ resource "aws_db_instance" "mariadb" {
   db_subnet_group_name      = aws_db_subnet_group.mariadb-subnet.name
   parameter_group_name      = aws_db_parameter_group.mariadb-parameters.name
   multi_az                  = "false" # set to true to have high availability: 2 instances synchronized with each other
+  publicly_accessible       = "true"
   vpc_security_group_ids    = [aws_security_group.allow-mariadb.id]
   storage_type              = "gp2"
   backup_retention_period   = 30                                          # how long you’re going to keep your backups
