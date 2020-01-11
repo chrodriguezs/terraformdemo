b842fdf754facbd7bcb2f1fac91809bf19257110
chrodriguezs <cdrs1991@gmail.com>
2020-01-10 20:28:11 -0500

    Add files via upload

--- lap.sh
+++ lap.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+sudo apt-get update
+
 ## Install AP
-sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
-sudo apt-get install libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-gd php5-intl php5-openssl php5-idn php-pear php5-imagick php5-mcrypt php5-mysql php5-ps php5-pspell php5-recode php5-xsl -y
+sudo apt-get install apache2 apache2-doc apache2-utils -y
+sudo apt-get install libapache2-mod-php7.0 php7.0 php7.0-common php7.0-curl php7.0-dev php7.0-gd php7.0-intl php7.0-openssl php7.0-idn php-pear php7.0-imagick php7.0-mcrypt php7.0-mysql php7.0-ps php7.0-pspell php7.0-recode php7.0-xsl -y
 
 
 # Permissions
@@ -10,11 +12,11 @@ sudo chown -R www-data:www-data /var/www
 
 # Enabling Mod Rewrite, required for WordPress permalinks and .htaccess files
 sudo a2enmod rewrite
-sudo php5enmod mcrypt
+sudo php7.0enmod mcrypt
 
 sudo service apache2 restart
 
 # Enabling Apache
 sudo systemctl enable apache2.service
 
-exit
+~.
