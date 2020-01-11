c210184ec71b230fd2908e42c9bfa798085bae90
chrodriguezs <cdrs1991@gmail.com>
2020-01-10 21:41:20 -0500

    Add files via upload

--- instance.tf
+++ instance.tf
@@ -15,9 +15,28 @@ resource "aws_instance" "example" {
       Name = "WebServer"
       Project = "DEMO_Ene20"
   }
+
+  connection {
+    type = "ssh"
+    host = self.public_ip
+    user = var.IaaC_Username
+    password = var.IaaC_Passphrase
+    private_key = var.IaaC_PrivateKey
+  }
+  
+  provisioner "file" {
+    source      = "lap.sh"
+    destination = "/tmp/lap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/lap.sh",
+      "/tmp/lap.sh",
+    ]
+  }
+
 }
-
-
   output "webserver_public_ip" {
       value = aws_instance.example.public_ip
   }
