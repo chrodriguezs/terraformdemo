6971264330e205fe0f1352ba8220f14c0e0c1ee9
chrodriguezs <cdrs1991@gmail.com>
2020-01-11 00:17:19 -0500

    Add files via upload

--- lap.sh
+++ lap.sh
@@ -7,5 +7,5 @@ echo "Installing PHP & Complements"
 sudo apt-get install -y php7.0 php7.0-gd php7.0-bcmath php7.0-intl php7.0-mbstring php7.0-curl
 echo "Enable rewrite & Restart and Enable Apache Service"
 sudo a2enmod rewrite
-sudo Service apache2 restart
-sudo systemclt enable apache2.service
+sudo service apache2 restart
+sudo systemctl enable apache2.service
