e0c5edcc04b255f74edfae9aaa10ffed9d4ec682
chrodriguezs <cdrs1991@gmail.com>
2020-01-11 19:52:37 -0500

    Add files via upload

--- lap.sh
+++ lap.sh
@@ -5,7 +5,7 @@ sleep 10
 echo "Installing Apache"
 sudo apt-get install -y apache2 apache2-bin apache2-utils
 echo "Installing PHP & Complements"
-sudo apt-get install -y php7.0 php7.0-gd php7.0-bcmath php7.0-intl php7.0-mbstring php7.0-curl
+sudo apt-get install -y libapache2-mod-php7.0 php7.0 php7.0-gd php7.0-bcmath php7.0-intl php7.0-mbstring php7.0-curl php7.0-mysql
 echo "Enable rewrite & Restart and Enable Apache Service"
 sudo a2enmod rewrite
 sudo service apache2 restart
